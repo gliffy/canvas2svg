a7cce0c167fdb5f478d5cca1e590e47985468840
Michael Deal <mudcube@gmail.com>
2018-07-05 16:52:41 -0700

    Fix issue with ctx.transform being overwritten with DOMMatrix

--- canvas2svg.js
+++ canvas2svg.js
@@ -330,6 +330,9 @@
         var keys = Object.keys(styleState), i, key;
         for (i=0; i<keys.length; i++) {
             key = keys[i];
+            if (key === 'transform') {
+                continue;
+            }
             this[key] = styleState[key];
         }
         this.__currentMatrix = new DOMMatrix(styleState.transform);
