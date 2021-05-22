dada4e25b0ba1370dd1e26226cae95c47ccd9b46
Zeno Zeng <zenoofzeng@gmail.com>
2021-05-23 00:58:30 +0800

    test: example/transform

---
+++ test/example/transform.js
@@ -0,0 +1,21 @@
+window.C2S_EXAMPLES['transform'] = function(ctx) {
+    // case 1
+    ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+    ctx.setTransform(1,1,0,1,0,0);
+    ctx.fillRect(0,0,100,100);
+
+    // case 2
+    ctx.resetTransform();
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, 50, 50);
+
+    // case 3
+    ctx.resetTransform();
+    ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
+    ctx.moveTo(0, 0);
+    ctx.lineTo(100, 0)
+    ctx.transform(2, 0, 0, 2, 0, 0)
+    ctx.lineTo(100, 100)
+    ctx.closePath()
+    ctx.fill()
+}
